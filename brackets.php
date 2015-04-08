91d57f355b3c488650240e7c3b419544e861da31
Amsterdamys <zarkovsky@yandex.ru>
2015-04-08 11:06:58 +0400

    Create brackets.php
    Create class Brackets

---
+++ brackets.php
@@ -0,0 +1,86 @@
+<?php
+
+class Brackets {
+
+    /**
+     * @var null
+     * container for brackets
+     */
+    public $brackets = null;
+
+    public function __construct(){
+        $this->prepareBrackets();
+
+        return false;
+    }
+
+    /**
+     * @return string
+     * validate brackets
+     */
+    public function validate(){
+        if($this->brackets === null) return $this->getResponse(3);
+
+        while ($this->brackets != ''){
+            $strlen_start   = strlen($this->brackets);
+
+            $this->brackets = preg_replace('/(\(\)|\[\]|\{\})/', '', $this->brackets);
+
+            $strlen_end     = strlen($this->brackets);
+
+            if($strlen_start == $strlen_end && $this->brackets != '') return $this->getResponse(2);
+        }
+
+        return $this->getResponse(1);
+    }
+
+    /**
+     * check brackets
+     */
+    public function prepareBrackets(){
+        if(isset($_GET['brackets'])){
+            $this->brackets = preg_replace('/[^\(\)\[\]\{\}]/', '', $_GET['brackets']);
+        }
+    }
+
+    /**
+     * @param bool $result
+     * @return string
+     * returns the colored resulting string
+     */
+    public function getResponse($result){
+        switch($result){
+            case 1:
+                $response = '<span style="color:green">String is correct</span>';
+                break;
+            case 2:
+                $response = '<span style="color:red">String is incorrect</span>';
+                break;
+            case 3:
+                $response = 'Brackets is empty';
+                break;
+        }
+
+        return $response;
+    }
+
+}
+
+$brackets = new Brackets();
+
+echo $brackets->validate();
+
+?>
+
+<html>
+<head>
+</head>
+<body>
+    <form action="">
+        <br>
+        <input name="brackets" value="<?=isset($_GET['brackets'])?$_GET['brackets']:'{}{{[[{}]]}[]}'?>" /><br>
+        <br>
+        <button>Отправить</button>
+    </form>
+</body>
+</html>
